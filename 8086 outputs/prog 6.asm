8a9424ed4483fae409b15ec9104e07948bff4060
arpitpatelSitapur <pa7238867@gmail.com>
2024-04-09 21:25:55 +0530

    Delete 8086 outputs/prog 6.asm

--- 8086 outputs/prog 6.asm
+++
@@ -1,5 +0,0 @@
-MOV AX, 1234 ; Load BCD value into AX register
-AAA           ; Adjust AX to valid BCD format
-
-; After AAA, AX will contain the binary equivalent of the BCD value 1234h
-                       
