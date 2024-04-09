ee667a17bc5cf121448f843fba7b7a1107f90154
arpitpatelSitapur <pa7238867@gmail.com>
2024-04-09 21:26:33 +0530

    Update prog 1.asm

--- 8086 programs/prog 1.asm
+++ 8086 programs/prog 1.asm
@@ -1,5 +1,5 @@
-MOV AX, 10h ; Load AX with a number
-MOV BX, 8h ; Load AX with a number
+MOV AX, 20h ; Load AX with a number
+MOV BX, 20h ; Load AX with a number
 MOV CX, AX  ; Move AX to CX
 ADD CX, BX  ; Perform ADD operation ; Result is in CX
 MOV DX, AX  ; Move AX to DX 
