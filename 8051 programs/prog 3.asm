f91c55f68773253c3355d05a6005a94777e56b6e
arpitpatelSitapur <pa7238867@gmail.com>
2024-04-11 16:43:47 +0530

    Created prog 3.asm

---
+++ 8051 programs/prog 3.asm
@@ -0,0 +1,6 @@
+mov r0,#60h ; Load r0 with the first number
+mov r1,#50h ; Load r1 with the second number
+mov a,r0    ; move r0 to a
+mov b,r1    ; move r1 to b
+mul ab      ; a:b = a*b ; result in a(low byte) and b(high byte)
+end
