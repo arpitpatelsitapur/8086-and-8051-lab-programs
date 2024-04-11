75109122a7ba67504371cf1ffa06c2ec3c1264c9
arpitpatelSitapur <pa7238867@gmail.com>
2024-04-11 16:50:28 +0530

    Update prog 2.asm

--- 8051 programs/prog 2.asm
+++ 8051 programs/prog 2.asm
@@ -1,6 +1,6 @@
 mov r0,#60h ; move first number to r0
 mov r1,#50h ; move second number to r1
-mov a,@r0    ; move r0 to accumulator
+mov a,r0    ; move r0 to accumulator
 subb a,r1   ; a=a-r1
-mov r2,@a    ; move result to r2
+mov r2,a    ; move result to r2
 end
