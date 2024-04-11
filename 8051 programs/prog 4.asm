a7f2c5a4b8eb813282da8966e874680dd8aef416
arpitpatelSitapur <pa7238867@gmail.com>
2024-04-11 17:54:02 +0530

    Updated prog 4.asm

--- 8051 programs/prog 4.asm
+++ 8051 programs/prog 4.asm
@@ -1,5 +1,5 @@
-mov r0,#60h ; Load r0 with the first number
-mov r1,#5h ; Load r1 with the second number
+mov r0,#60h ; Load r0 with numerator
+mov r1,#5h ; Load r1 with denominator
 mov a,r0    ; move r0 to a
 mov b,r1    ; move r1 to b
 div ab    ; a=a/b 
