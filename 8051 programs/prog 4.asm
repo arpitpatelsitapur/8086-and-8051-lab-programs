5911d52a258013b69f414329d8772a7d4f1d93a4
arpitpatelSitapur <pa7238867@gmail.com>
2024-04-19 21:55:18 +0530

    Updated prog 4.asm

--- 8051 programs/prog 4.asm
+++ 8051 programs/prog 4.asm
@@ -1,7 +1,14 @@
-mov r0,#60h ; Load r0 with numerator
-mov r1,#5h ; Load r1 with denominator
+; Load 16-bit numerator into r0 and r2
+mov r0, #60h   ; Load lower 8 bits of numerator
+mov r1, #0     ; Load upper 8 bits of numerator
+; Load 8-bit divisor into r1
+mov r2, #5h    ; Load lower 8 bits of divisor
+
 mov a,r0    ; move r0 to a
-mov b,r1    ; move r1 to b
-div ab    ; a=a/b 
+mov b,r2    ; move r2 to b
+
+; Divide 16-bit number by 8-bit divisor
+div ab         ; a=a/b 
 ; result in a(quotient) and b(remainder)
+
 end
